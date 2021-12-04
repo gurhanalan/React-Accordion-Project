9c634f6e192e31e25372e9d279451f553a56df47
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-04 13:19:45 +0300

    Updates

---
+++ precache-manifest.c232a3e583540395cc5442dd228b870f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fc194bf216fed412ab779933a5b518",
+    "url": "/React-Accordion-Project/index.html"
+  },
+  {
+    "revision": "5785daaac42fa5cb5b49",
+    "url": "/React-Accordion-Project/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/React-Accordion-Project/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Accordion-Project/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5785daaac42fa5cb5b49",
+    "url": "/React-Accordion-Project/static/js/main.890d3a4b.chunk.js"
+  },
+  {
+    "revision": "06c684b8a2a6bb689e0f",
+    "url": "/React-Accordion-Project/static/js/runtime-main.bbb42426.js"
+  }
+]);
